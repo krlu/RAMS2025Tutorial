31bf1bf7c56d61dca340f6aac7de00740f3b134a
Kenny Lu <klu@cra.com>
2024-10-22 12:21:14 -0400

    adding static model example

--- src/static_model.jl
+++ src/static_model.jl
@@ -5,36 +5,44 @@ using Scruff.RTUtils
 using Scruff.Models
 using Scruff.SFuncs
 using Scruff.Algorithms
-import Scruff: make_initial, make_transition
 
 needsMaintenanceSF = Cat([true, false], [0.5, 0.5])
+
 tempSF = Chain(Tuple{Bool}, Float64, tuple -> begin 
     nmBool = tuple[1]
     if nmBool 
         Uniform(0.0, 300.0)
     else
-        Normal(207.5, 4.0) # taken from domain expertise
+        Normal(207.5, 4.0)
     end
 end)
 
 nmModel = SimpleModel(needsMaintenanceSF)
-nmVar = nmModel(:needsMaintenance)
+needsMaintenance = nmModel(:needsMaintenance)
 
 tempModel = SimpleModel(tempSF)
-tempVar = tempModel(:temp)
+temperature = tempModel(:temp)
 
-variables = [nmVar, tempVar]
-graph = VariableGraph(tempVar => [nmVar])
+variables = [temperature, needsMaintenance]
+graph = VariableGraph(temperature => [needsMaintenance])
 net = InstantNetwork(variables, graph)
 runtime = Runtime(net)
-alg = LW(100000)
+numSamples = 10000
+alg = LW(numSamples)
 
-observedTemp = 195.0
+observedTemp = 200.0
 score = FunctionalScore{Float64}(temp -> (begin
     1.0/abs(temp - observedTemp)^2
 end))
-parts = infer(alg, runtime, Dict{Symbol, Score}(:temp => score))
-prob = probability(parts, x -> begin 
+evidence = Dict{Symbol, Score}(:temp => score)
+sampledResults = infer(alg, runtime, evidence)
+
+probTemp = probability(sampledResults, x -> begin 
+    199 < x[:temp] < 201
+end)
+
+probNeedsMaintenance = probability(sampledResults, x -> begin 
     x[:needsMaintenance]
 end)
-println(prob)
+println(probTemp)
+println(probNeedsMaintenance)
